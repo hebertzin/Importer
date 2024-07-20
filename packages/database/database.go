d578d522261d5182a8daeffeaeef526f946de0dd
hebertsanto <hebertsantos0704@gmail.com>
2024-07-20 09:10:35 -0300

    Feat : Create model suppliers and add env config in package database

--- packages/database/database.go
+++ packages/database/database.go
@@ -1,11 +1,10 @@
 package database
 
 import (
+	"enube-challenge/packages/config"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,24 +12,11 @@ import (
 var db *gorm.DB
 
 func ConnectDatabase() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	user := os.Getenv("USER_DATABASE")
-	password := os.Getenv("USER_PASSWORD")
-	database := os.Getenv("DATABASE")
-	port := os.Getenv("PORT")
-	host := os.Getenv("HOST")
-
-	if user == "" || password == "" || database == "" || port == "" || host == "" {
-		log.Fatal("One or more required environment variables are not set")
-	}
-
+	cfg := config.LoadConfig()
 	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s host=%s sslmode=disable TimeZone=Asia/Shanghai",
-		user, password, database, port, host)
+		cfg.User, cfg.Password, cfg.Database, cfg.Port, cfg.Host)
 
+	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
--- packages/models/users.go
+++ packages/models/users.go
@@ -1,8 +1,16 @@
 package models
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type Users struct {
-	ID       uint   `gorm:"primaryKey"`
-	Username string `gorm:"not null"`
-	Password string `gorm:"not null"`
-	Email    string `gorm:"unique;not null"`
+	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"not null"`
+	Password  string `gorm:"not null"`
+	Email     string `gorm:"unique;not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
