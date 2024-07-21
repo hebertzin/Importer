9e118f22fd4f04f941085494f5331086cd228421
hebertsanto <hebertsantos0704@gmail.com>
2024-07-21 12:52:50 -0300

    Feat : Create suppliers controllers and ajust routes

--- packages/routes/supplier.go
+++ packages/routes/supplier.go
@@ -2,17 +2,21 @@ package routes
 
 import (
 	"enube-challenge/packages/controllers"
+	"enube-challenge/packages/repository"
 	"enube-challenge/packages/services"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func Importer(router *gin.Engine, db *gorm.DB) {
-	supplierService := services.NewSupplierService(db)
+func Suppliers(router *gin.Engine, db *gorm.DB) {
+	supplierRepository := repository.NewSupplierRepository(db)
+	supplierService := services.NewSupplierService(supplierRepository)
 	supplierController := controllers.NewSupplierController(supplierService)
 
 	supplierGroup := router.Group("/api/v1")
 	{
-		supplierGroup.POST("/import/suppliers", supplierController.ImportSuppliersHandler)
+		supplierGroup.POST("/suppliers/import", supplierController.ImportSuppliersHandler)
+		supplierGroup.GET("/suppliers", supplierController.FindSuppliersHandler)
+		supplierGroup.GET("/suppliers/:id", supplierController.FindSupplierById)
 	}
 }
