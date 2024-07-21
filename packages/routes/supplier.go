744d896f199fe78b7f6a60466451a6dad7797bf8
hebertsanto <hebertsantos0704@gmail.com>
2024-07-21 16:50:10 -0300

    Feat : Add middleware in suppliers routes

--- packages/routes/supplier.go
+++ packages/routes/supplier.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"enube-challenge/packages/controllers"
+	middleware "enube-challenge/packages/middlewares"
 	"enube-challenge/packages/repository"
 	"enube-challenge/packages/services"
 	"github.com/gin-gonic/gin"
@@ -9,14 +10,17 @@ import (
 )
 
 func Suppliers(router *gin.Engine, db *gorm.DB) {
+
+	jwtService := services.NewJWTService()
+
 	supplierRepository := repository.NewSupplierRepository(db)
 	supplierService := services.NewSupplierService(supplierRepository)
 	supplierController := controllers.NewSupplierController(supplierService)
 
 	supplierGroup := router.Group("/api/v1")
 	{
-		supplierGroup.POST("/suppliers/import", supplierController.ImportSuppliersHandler)
-		supplierGroup.GET("/suppliers", supplierController.FindSuppliersHandler)
-		supplierGroup.GET("/suppliers/:id", supplierController.FindSupplierById)
+		supplierGroup.POST("/suppliers/import", middleware.AuthMiddleware(jwtService), supplierController.ImportSuppliersHandler)
+		supplierGroup.GET("/suppliers", middleware.AuthMiddleware(jwtService), supplierController.FindSuppliersHandler)
+		supplierGroup.GET("/suppliers/:id", middleware.AuthMiddleware(jwtService), supplierController.FindSupplierById)
 	}
 }
